7bb54517bc7806dbbccaa2244ab4dde723970a9c
myungho <skqoaudgh2@hanmail.net>
2019-08-15 13:11:03 +0900

    Phonebook 모델 스키마 추가

---
+++ models/phonebook.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const phonebookSchema = new Schema({
+    Creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    Name: {
+        type: String,
+        required: true
+    },
+    Number: {
+        type: String,
+        required: true
+    },
+    Relation: {
+        type: String,
+        required: false
+    },
+    Email: {
+        type: String,
+        required: false
+    },
+    Comment: {
+        type: String,
+        required: false
+    },
+});
+
+module.exports = mongoose.model('Phonebook', phonebookSchema);
