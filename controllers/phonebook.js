288c0d10c143ea0e354ae76430fa67d107325fdf
myungho <skqoaudgh2@hanmail.net>
2019-08-18 14:54:55 +0900

    API code prettify

--- controllers/phonebook.js
+++ controllers/phonebook.js
@@ -8,7 +8,6 @@ function isNumeric(value) {
 module.exports = {
     createItem: async (req, res, next) => {
         try {
-            const userId = req.userId;
             if(!req.body.name || !req.body.number || !req.body.name.trim() || !req.body.number.trim()) {
                 return res.status(400).json({error: 'Unexpected JSON input'});
             }
@@ -17,9 +16,8 @@ module.exports = {
                 req.body.email = req.body.email.trim();
             }
 
-            const id = req.id;
             const inputPhonebook = new Phonebook({
-                Creator: id,
+                Creator: req.userId,
                 Name: req.body.name,
                 Number: req.body.number.trim(),
                 Relation: req.body.relation?(req.body.relation):'None',
@@ -27,52 +25,46 @@ module.exports = {
                 Comment: req.body.comment?(req.body.comment):'None',
             });
             const savedPhonebook = await inputPhonebook.save();
-            if(inputPhonebook == savedPhonebook) {
-                return res.status(201).json(savedPhonebook);
-            }
-            else {
+            if(inputPhonebook != savedPhonebook) {
                 return res.status(204).json({error: 'Not expected value is found'});
-            }     
+            }
+
+            res.status(201).json(savedPhonebook);    
         }
         catch(err) {
             console.error(err);
-            return res.status(500).json({error: 'Internal server error'});           
+            res.status(500).json({error: 'Internal server error'});           
         }
     },
 
     getPhonebooks: async (req, res, next) => {
         try {
-            const userId = req.userId;
-            const phonebooks = await Phonebook.find({Creator: userId});
-            if(phonebooks && phonebooks.length > 0) {
-                return res.status(200).json(phonebooks);
-            }
-            else {
+            const phonebooks = await Phonebook.find({Creator: req.userId});
+            if(!phonebooks || phonebooks.length == 0) {
                 return res.status(404).json({error: 'No item to serve'});
-            }   
+            }
+
+            res.status(200).json(phonebooks); 
         }
         catch(err) {
             console.error(err);
-            return res.status(500).json({error: 'Internal server error'});                
+            res.status(500).json({error: 'Internal server error'});                
         }
     },
 
     getPhonebook: async (req, res, next) => {
         try {
-            const userId = req.userId;
             const itemId = req.params.Itemid;
-            if(mongoose.Types.ObjectId.isValid(itemId)) {
-                const phonebook = await Phonebook.findOne({_id: itemId, Creator: userId});
-                if(phonebook) {
-                    return res.status(200).json(phonebook);
-                }
-                else {
-                    return res.status(404).json({error: 'No item to serve'});
-                }
-            }
-            else {
+            if(!mongoose.Types.ObjectId.isValid(itemId)) {
                 return res.status(400).json({error: 'Item ID incorrect'});
-            }             
+            }
+
+            const phonebook = await Phonebook.findOne({_id: itemId, Creator: req.userId});
+            if(!phonebook) {
+                return res.status(404).json({error: 'No item to serve'});
+            }
+
+            res.status(200).json(phonebook);          
         }
         catch(err) {
             console.error(err);
@@ -82,92 +74,84 @@ module.exports = {
 
     updatePhonebook: async (req, res, nexdt) => {
         try {
-            const userId = req.userId;
             const itemid = req.params.Itemid;
-            if(mongoose.Types.ObjectId.isValid(itemId)) {
-                const phonebook = await Phonebook.findOne({_id: itemid, Creator: userId});
-    
-                if(phonebook) {
-                    if(req.body.name) 
-                        phonebook.Name = req.body.name;
-                    if(req.body.number && req.body.number.trim())
-                        phonebook.Number = req.body.number.trim();
-                    if(req.body.relation)
-                        phonebook.relation = req.body.relation;
-                    if(req.body.email && req.body.email.trim())
-                        phonebook.Email = req.body.email.trim();
-                    if(req.body.comment)
-                        phonebook.Comment = req.body.comment;
-                    
-                    try {
-                        const updatedPhonebook = await phonebook.save();
-                        return res.status(200).json(updatedPhonebook);
-                    }
-                    catch(err) {
-                        console.error(err);
-                        return res.status(500).json({error: 'Internal server error'});                   
-                    }
-                }
-                else {
-                    return res.status(404).json({error: 'No user to update'});
-                }
-            }
-            else {
+            if(!mongoose.Types.ObjectId.isValid(itemId)) {
                 return res.status(400).json({error: 'Item ID incorrect'});
-            }  
+            }
+
+            const phonebook = await Phonebook.findOne({_id: itemid, Creator: req.userId});
+            if(!phonebook) {
+                return res.status(404).json({error: 'No user to update'});
+            }
+
+            if(req.body.name) 
+                phonebook.Name = req.body.name;
+            if(req.body.number && req.body.number.trim())
+                phonebook.Number = req.body.number.trim();
+            if(req.body.relation)
+                phonebook.relation = req.body.relation;
+            if(req.body.email && req.body.email.trim())
+                phonebook.Email = req.body.email.trim();
+            if(req.body.comment)
+                phonebook.Comment = req.body.comment;
+            
+            try {
+                const updatedPhonebook = await phonebook.save();
+                res.status(200).json(updatedPhonebook);
+            }
+            catch(err) {
+                console.error(err);
+                res.status(500).json({error: 'Internal server error'});                   
+            }
         }
         catch(err) {
             console.error(err);
-            return res.status(500).json({error: 'Internal server error'}); 
+            res.status(500).json({error: 'Internal server error'}); 
         }
     },
 
     deletePhonebook: async (req, res, next) => {
         try {
-            const userId = req.userId;
             const itemId = req.params.Itemid;
-            if(mongoose.Types.ObjectId.isValid(itemId)) {
-                const deletedPhonebook = await Phonebook.findOneAndDelete({_id: itemId, Creator: userId});
-                if(deletedPhonebook) {
-                    return res.status(404).json(deletedPhonebook);
-                }
-                else {
-                    return res.status(404).json({error: 'No user to delete'});
-                }
-            }
-            else {
+            if(!mongoose.Types.ObjectId.isValid(itemId)) {
                 return res.status(400).json({error: 'Item ID incorrect'});
-            }         
+            }
+
+            const deletedPhonebook = await Phonebook.findOneAndDelete({_id: itemId, Creator: req.userId});
+            if(!deletedPhonebook) {
+                return res.status(404).json({error: 'No user to delete'});
+            }
+
+            res.status(404).json(deletedPhonebook);     
         }
         catch(err) {
             console.error(err);
-            return res.status(500).json({error: 'Internal server error'});           
+            res.status(500).json({error: 'Internal server error'});           
         }
     },
 
     searchPhonebook: async (req, res, next) => {
         try {
-            const userId = req.userId;
             if(req.body.terms) {
-                const name = req.body.terms.name?req.body.terms.name:'';
-                const number = req.body.terms.number?req.body.terms.number:'';
-                const relation = req.body.terms.relation?req.body.terms.relation:'';
-
-                const nameReg = new RegExp(name.replace(/\s+/g,"\\s+"), "gi");
-                const numberReg = new RegExp(number.replace(/\s+/g,"\\s+"), "gi");
-                const relationReg = new RegExp(relation.replace(/\s+/g,"\\s+"), "gi");
-
-                const phonebooks = await Phonebook.find({$and: [
-                    {Name: {"$regex": nameReg}},
-                    {Number: {"$regex": numberReg}},
-                    {Relation: {"$regex": relationReg}}
-                ]});
-                
-                return res.status(200).json(phonebooks);
-            }
-            else {
                 return res.status(400).json({error: 'Unexpected JSON input'});
             }
+            
+            const name = req.body.terms.name?req.body.terms.name:'';
+            const number = req.body.terms.number?req.body.terms.number:'';
+            const relation = req.body.terms.relation?req.body.terms.relation:'';
+
+            const nameReg = new RegExp(name.replace(/\s+/g,"\\s+"), "gi");
+            const numberReg = new RegExp(number.replace(/\s+/g,"\\s+"), "gi");
+            const relationReg = new RegExp(relation.replace(/\s+/g,"\\s+"), "gi");
+
+            const phonebooks = await Phonebook.find({$and: [
+                {Name: {"$regex": nameReg}},
+                {Number: {"$regex": numberReg}},
+                {Relation: {"$regex": relationReg}},
+                {Creator: req.userId}
+            ]});
+            
+            res.status(200).json(phonebooks);
         }
         catch(err) {
             console.error(err);
--- controllers/user.js
+++ controllers/user.js
@@ -31,7 +31,7 @@ module.exports = {
             }
 
             const searchedUser = await User.find({$or: [{ID: req.body.id}, {Nickname: req.body.nickname}]});
-            if(searchedUser.length) {
+            if(searchedUser.length > 0) {
                 return res.status(409).json({error: 'ID or Nickname is already exist'});
             }
 
@@ -42,50 +42,47 @@ module.exports = {
                 Comment: req.body.comment
             });
             const savedUser = await inputUser.save();
-            if(inputUser == savedUser) {
-                return res.status(201).json(savedUser);
-            }
-            else {
+            if(inputUser != savedUser) {
                 return res.status(204).json({error: 'Not expected value is found'});
             }
+
+            res.status(201).json(savedUser);
         }
         catch(err) {
             console.error(err);
-            return res.status(500).json({error: 'Internal server error'});
+            res.status(500).json({error: 'Internal server error'});
         }
     },
 
     getUsers: async (req, res, next) => {
         try {
             const users = await User.find();
-            if(users) {
-                return res.status(200).json(users);
-            }
-            else {
+            if(!users) {
+                
                 return res.status(404).json({error: 'No users to serve'});
             }
+
+            res.status(200).json(users);
         }
         catch(err) {
             console.error(err);     
-            return res.status(500).json({error: 'Internal server error'});  
+            res.status(500).json({error: 'Internal server error'});  
         }
     },
 
     getUser: async (req, res, next) => {
         try {
             const userId = req.params.userId;
-            if(mongoose.Types.ObjectId.isValid(userId)) {
-                const user = await User.findById(userId);
-                if(user) {
-                    return res.status(200).json(user);
-                }
-                else {
-                    return res.status(404).json({error: 'No user to serve'});
-                }
-            }
-            else {
+            if(!mongoose.Types.ObjectId.isValid(userId)) {
                 return res.status(400).json({error: 'User ID incorrect'});
             }
+
+            const user = await User.findById(userId);
+            if(!user) {
+                return res.status(404).json({error: 'No user to serve'});
+            }
+
+            res.status(200).json(user);
         }
         catch(err) {
             console.error(err);     
@@ -96,67 +93,63 @@ module.exports = {
     updateUser: async (req, res, next) => {
         try {
             const userId = req.params.userId;
-            if(mongoose.Types.ObjectId.isValid(userId)) {
-                const user = await User.findById(userId);
-                if(user) {
-                    const searchedUser = await User.find({
-                        $and: [
-                            { $or: [{ID: req.body.id}, {Nickname: req.body.nickname}] },
-                            { _id: {$ne: user._id} }
-                        ]});
-                    if(searchedUser.length) {
-                        return res.status(409).json({error: 'ID or Nickname is already exist'});
-                    }
-    
-                    if(req.body.password && req.body.password.trim())
-                        user.Password = req.body.password.trim();
-                    if(req.body.nickname)
-                        user.Nickname = req.body.nickname;
-                    if(req.body.comment)
-                        user.Comment = req.body.comment;
-    
-                    try {
-                        const updatedUser = await user.save();
-                        return res.status(200).json(updatedUser);
-                    }
-                    catch(err) {
-                        console.error(err);
-                        return res.status(500).json({error: 'Internal server error'});                  
-                    }
-                }
-                else {
-                    return res.status(404).json({error: 'No user to update'});
-                }
-            }
-            else {
+            if(!mongoose.Types.ObjectId.isValid(userId)) {
                 return res.status(400).json({error: 'User ID incorrect'});
+            }
+
+            const user = await User.findById(userId);
+            if(!user) {
+                return res.status(404).json({error: 'No user to update'});
+            }
+
+            const searchedUser = await User.find({
+                $and: [
+                    { $or: [{ID: req.body.id}, {Nickname: req.body.nickname}] },
+                    { _id: {$ne: user._id} }
+                ]});
+            if(searchedUser.length > 0) {
+                return res.status(409).json({error: 'ID or Nickname is already exist'});
+            }
+
+            if(req.body.password && req.body.password.trim())
+                user.Password = req.body.password.trim();
+            if(req.body.nickname)
+                user.Nickname = req.body.nickname;
+            if(req.body.comment)
+                user.Comment = req.body.comment;
+
+            try {
+                const updatedUser = await user.save();
+                res.status(200).json(updatedUser);
+            }
+            catch(err) {
+                console.error(err);
+                res.status(500).json({error: 'Internal server error'});                  
             }
         }
         catch(err) {
             console.error(err);
-            return res.status(500).json({error: 'Internal server error'});
+            res.status(500).json({error: 'Internal server error'});
         }
     },
 
     deleteUser: async (req, res, next) => {
         try {
             const userId = req.params.userId;
-            if(mongoose.Types.ObjectId.isValid(userId)) {
-                const deletedUser = await User.findOneAndDelete({_id: userId});
-                if(deletedUser) {
-                    return res.status(404).json(deletedUser);
-                }
-                else {
-                    return res.status(404).json({error: 'No user to delete'});
-                }
-            }
-            else {
+            if(!mongoose.Types.ObjectId.isValid(userId)) {
                 return res.status(400).json({error: 'User ID incorrect'});
             }
+
+            const deletedUser = await User.findOneAndDelete({_id: userId});
+            if(!deletedUser) {
+                return res.status(404).json({error: 'No user to delete'});
+            }
+
+            res.status(404).json(deletedUser);
         }
         catch(err) {
             console.error(err);
-            return res.status(500).json({error: 'Internal server error'});           
+            res.status(500).json({error: 'Internal server error'});           
         }
     }
 }
