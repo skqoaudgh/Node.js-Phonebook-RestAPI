a7460aff5e28d1387d29c31ebe612fb95eb9657c
myungho <skqoaudgh2@hanmail.net>
2019-08-22 17:05:57 +0900

    gzip webpack compression 적용. GET favicon.ico 요청은 건너띄도록 수정

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const logger = require('morgan');
+const compression = require('compression');
 
 const Auth = require('./middleware/auth');
 
@@ -9,12 +10,16 @@ const authRouter = require('./routes/auth');
 
 const app = express();
 
+app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(logger('dev'));
 
-app.use(Auth);
+app.use('/favicon.ico', (req, res, next) => {
+    next();
+});
 
+app.use(Auth);
 app.use('/auth', authRouter);
 app.use('/users', userRouter);
 app.get('/', (req, res ,next) => {
